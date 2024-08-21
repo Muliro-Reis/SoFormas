39be360ac2ac2d05fc990c364864e87b79cc5359
murilo.schiavotto@gmail.com <murilo.schiavotto@gmail.com>
2024-08-21 14:06:04 -0300

    Alterando a seleção da combo e Botão Criar

--- WfaFormas/Form1.cs
+++ WfaFormas/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,66 @@ namespace WfaFormas
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void cmbForma_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbForma.Text)
+            {
+                case "Quadrado":
+                    SelecionarQuadrado();
+                    break;
+                case "Triangulo":
+                    SelecionarTriangulo();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SelecionarQuadrado()
+        {
+            ExibirBase(true);
+            ExibirAltura(false);
+            lblRaio.Visible = txtRaio.Visible = false;
+            cmbTriangulo.Visible = false;
+        }
+
+        private void ExibirBase(bool visivel)
+        {
+            lblBase.Visible = txtBase.Visible = visivel;
+        }
+
+        private void SelecionarTriangulo()
+        {
+            ExibirBase(true);
+            ExibirAltura(true);
+            lblRaio.Visible = txtRaio.Visible = false;
+            cmbTriangulo.Visible = true;
+        }
+
+        private void ExibirAltura(bool visivel)
+        {
+            lblAltura.Visible = txtAltura.Visible = visivel;
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+            if (cmbForma.Text.Equals("Quadrado"))
+            {
+                FormaGeometrica quadrado = new Quadrado()
+                {
+                    Lado = Convert.ToDouble(txtBase.Text)
+                };
+                cmbObjetos.Items.Add(quadrado);
+            }
+        }
+
+        private void cmbObjetos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FormaGeometrica obj = cmbObjetos.SelectedItem as FormaGeometrica;
+            txtArea.Text = obj.CalcularArea().ToString();
+            txtPerimetro.Text = obj.CalcularPerimetro().ToString();
         }
     }
 }
